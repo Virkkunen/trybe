7108cfe3b2820cdb67f795a7446440629f18d23b
Virkkunen <vrkknn@vrkknn.net>
2022-08-09 13:07:35 -0300

    Add exercise fundamentos-01-01

--- fundamentos/01-unix-e-bash/01-unix-e-bash-parte-1/exercicio-parte-1.sh
+++ fundamentos/01-unix-e-bash/01-unix-e-bash-parte-1/exercicio-parte-1.sh
@@ -5,3 +5,10 @@ cp trybe.txt trybe_backup.txt
 mv trybe.txt trybe2.txt
 mkdir backup
 mv trybe_backup.txt backup/
+mkdir backup2
+mv backup/trybe_backup.txt backup2/
+rm -rf backup/
+mv backup2 backup
+pwd && ls -hal
+rm -rf backup/
+clear
