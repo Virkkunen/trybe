2d3dbe772975736286c9b50114c8087817156cd7
Virkkunen <vrkknn@vrkknn.net>
2022-08-18 18:15:48 -0300

    Exercícios de operadores matemáticos

---
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/exercicio-4.1.1.js
@@ -0,0 +1,38 @@
+// 
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Faça programas para:
+//
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+//
+const a = 5;
+const b = 4;
+let resultado;
+
+function adicao (a, b) {
+  resultado = a + b;
+  console.log(resultado);
+}
+
+function subracao (a, b) {
+  resultado = a - b;
+  console.log(resultado);
+}
+
+function multiplicacao (a, b) {
+  resultado = a * b;
+  console.log(resultado);
+}
+
+function divisao (a, b) {
+  resultado = a / b;
+  console.log(resultado);
+}
+
+function modulo (a, b) {
+  resultado = a % b;
+  console.log(resultado);
+}
+
