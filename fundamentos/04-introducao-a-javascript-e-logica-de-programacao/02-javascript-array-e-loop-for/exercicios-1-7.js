beb0ccf217b3d56f4e91c5a5ad9499fa912d5ec8
Virkkunen <vrkknn@vrkknn.net>
2022-08-19 14:51:00 -0300

    Exercicio 4.2.1

---
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/02-javascript-array-e-loop-for/exercicios-1-7.js
@@ -0,0 +1,9 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+function exercicio01 (numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+  }
+}
+
+exercicio01(numbers);
