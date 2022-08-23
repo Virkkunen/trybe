3e1d25de8f4a81ecee46c7252a0a9a403ec7e612
Virkkunen <vrkknn@vrkknn.net>
2022-08-23 15:57:35 -0300

    exercicio 5

--- fundamentos/04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/parte-1.js
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/parte-1.js
@@ -43,3 +43,27 @@ function forInValues (obj) {
 }
 
 forInValues(info);
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves.
+
+function newObject () {
+  let info2 = {};
+  info2.personagem = "Tio Patinhas";
+  info2.origem = "Christmas on Bear Mountain, Dell's Four Color Comics #178";
+  info2.nota = "O último MacPatinhas";
+  info2.recorrente = "Sim";
+  return info2;
+}
+
+function infos () {
+  info2 = newObject();
+  for (key in info) {
+    if (info[key] === info2[key]) {
+      console.log(`Ambos recorrentes`);
+    } else {
+      console.log(`${info[key]} e ${info2[key]}`);
+    }
+  }
+}
+
+infos();
