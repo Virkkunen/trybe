ff1e4662c074353fcedd22dea5cb2bdb5d5bbdde
Virkkunen <vrkknn@vrkknn.net>
2022-08-24 10:14:54 -0300

    exercício 5, XGH edition

--- fundamentos/04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/parte-2.js
+++ fundamentos/04-introducao-a-javascript-e-logica-de-programacao/04-javascript-objetos-e-funcoes/parte-2.js
@@ -21,17 +21,42 @@ console.log(verificaPalindromo(palavra), '\n');
 // Valor esperado no retorno da função: 2.
 
 const arrayInteiros = [2, 3, 2, 5, 8, 2, 3];
+const arrayGrande = [200, 300, 200, 50, 800, 200, 300];
+
 
 function valorRepetido (arr) {
-  let obj = {};
+  // XGH pesado nesse exercício
+  let count = [];
+  let biggest = 0, limit = 0;
 
-  for (i in arr) {
-    obj[arr[i]] = 1;
+  // coloca os valores do array como chaves do objeto
+  // for (i in arr) {
+  //   obj[arr[i]] = 1;
+  // }
+
+  // definindo qual o limite do array
+  // não encontrará boas práticas nessa função
+  for (let i of arr) if (i > limit) limit = i;
+
+  console.log(limit);
+
+  // cria array com 0s, indice = numero da array - 1
+  for (let i = 0; i <= limit; i += 1) {
+    count.push(0);
   }
 
-  console.log(obj);
+  // +1 se o numero repete (não tem 1 no array, então index 0 = 0; tem 3 2, então index 1 = 3)
+  for (let key of arr) {
+    count[key - 1] += 1;
+  }
+  console.log(count)
+
+  // verifica qual é o maior núemro do array novo
+  for (let key of count) biggest = key > biggest ? key : biggest;
+
+  // e ai retorna o índice do maior numero desse array + 1
+  return (count.indexOf(biggest) + 1);
+
 }
 
-valorRepetido(arrayInteiros);
-
-// desisto temporariamente
+console.log(valorRepetido(arrayGrande));
