3358e0c5e09b15fd66c86db19ce6a64f2604cd7e
Virkkunen <vrkknn@vrkknn.net>
2022-09-28 18:30:56 -0300

    exercicios/8.5

---
+++ fundamentos/08-higher-order-functions-do-js-es6/05-js-es6-spread-rest-destructuring/exercise2.js
@@ -0,0 +1,5 @@
+// Crie uma função sum que dado um número ilimitado de parâmetros retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6) o número 15 deve ser retornado.
+
+const sum = (...args) => args.reduce((acc, curr) => acc + curr);
+
+console.log(sum(4, 5, 6));
