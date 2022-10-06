103f59855e86a8a8e700a7754d385181ebb03454
Virkkunen <vrkknn@vrkknn.net>
2022-10-06 18:57:03 -0300

    exercicios/1.1

--- frontend/01-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/02.js
+++ frontend/01-javascript-e-testes-assincronos/01-javascript-assincrono-e-callbacks/02.js
@@ -5,10 +5,27 @@ const getMarsTemperature = () => {
   return Math.floor(Math.random() * maxTemperature);
 };
 
+const toFahrenheit = (degreeCelsius) => (degreeCelsius * (9 / 5)) + 32;
+
+const temperatureInFahrenheit = (temperature) =>
+  console.log(`Atualmente está ${toFahrenheit(temperature)}ºF em Marte`);
+
+const greet = (temperature) =>
+  console.log(`Olá! Curiosity aqui. Nesse momento está ${temperature}ºC em Marte`);
+
 // crie a função sendMarsTemperature abaixo
 
-const sendMarsTemperature = () => {
-  setTimeout(() => console.log(`A temperatura de Marte é: ${getMarsTemperature()} graus celsius`), messageDelay());
+const sendMarsTemperature = (option) => {
+  const celsius = getMarsTemperature();
+  if (!option) setTimeout(() => console.log(`A temperatura de Marte é: ${celsius} graus celsius`), messageDelay());
+  if (option === temperatureInFahrenheit) {
+    setTimeout(() => temperatureInFahrenheit(celsius), messageDelay());
+  }
+  if (option === greet) {
+    setTimeout(() => greet(celsius), messageDelay());
+  };
 }; // Imprime "A temperatura de Marte é: 20 graus celsius", por exemplo
 
 sendMarsTemperature();
+sendMarsTemperature(temperatureInFahrenheit);
+sendMarsTemperature(greet);
