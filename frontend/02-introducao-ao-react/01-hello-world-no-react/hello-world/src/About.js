d5e91c6a3d6c3058fadd824c9ade0ae173d1e07b
Virkkunen <vrkknn@vrkknn.net>
2022-10-19 19:52:04 -0300

    fixação

---
+++ frontend/02-introducao-ao-react/01-hello-world-no-react/hello-world/src/About.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Hello World!</h1>
+        <p>lorem ipsum pri dolor</p>
+        <h2>Header #2</h2>
+        <ul>
+          <li>01</li>
+          <li>02</li>
+          <li>03</li>
+        </ul>
+      </section>
+    )
+  }
+}
+
+export default About;
